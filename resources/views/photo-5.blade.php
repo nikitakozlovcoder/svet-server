cfe315bf8eadba376a1e20a983e18490a3a8bd8b
unknown <nikiton2606@mail.ru>
2019-08-15 12:27:13 +0300

    Initial commit

---
+++ resources/views/photo-5.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Галерея</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory-->
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="icon" href="icon.png">
+    <style>
+        .product_img{
+            height: 100%;
+            display: flex;
+            padding-top: 75%;
+            flex-direction: column;
+            position: relative;
+        }
+        .product_img img {
+            margin: auto;
+            top: 0;
+            left: 0;
+            object-fit: cover;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+        }
+
+    </style>
+
+</head>
+<body style="min-height: 100vh; display: flex; flex-direction: column;">
+<header>
+    <nav class="nav" style="background: white !important">
+        <div class="wrap">
+            <div class="logo" style="color: #515562 !important;display: flex;"><div><a href='../photos'><i class="fa fa-arrow-left" style="font-size:16px; padding-right: 25px;" ></i></a>	</div>	<span>SVE</span>T-SKG</div>
+            <div class="toggle">
+                <div class="toggle_nav">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+            <div class="links" style="color: #2c3e50 !important">
+                <div class="links_wrap">
+                    <hr><a class=" link" href="/#home" style="color: #515562;" >Главная</a>
+                    <a class=" link" href="/#about" style="color: #515562;">О нас</a>
+                    <a class=" link" href="/#products" style="color: #515562;">Продукция</a>
+                    <a class=" link" href="/#work" style="color: #515562;">Работы</a>
+                    <a class=" link" href="/#service" style="color: #515562;">Возможности</a>
+                    <a class=" link" href="/#contact" style="color: #515562;">Связаться</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
+<div class="wrap main-wrap">
+    <div class="section_title">
+        <h2>Дзержинского 177.</h2>
+        <div class="section_title_text">Освещение придомовой территории.</div>
+    </div>
+    <div class="products-items-grid">
+        <div class="product_col products-items-grid_col">
+            <div class="product_img"><img src="../img/products-work/14.jpg" class="work-img expand-img" data-full="../img/products-work/full/14.jpg"></div>
+
+        </div>
+        <div class="product_col products-items-grid_col">
+            <div class="product_img"><img src="../img/products-work/15.jpg" class="work-img expand-img" data-full="../img/products-work/full/15.jpg"></div>
+
+        </div>
+        <div class="product_col products-items-grid_col">
+            <div class="product_img"><img src="../img/products-work/16.jpg" class="work-img expand-img" data-full="../img/products-work/full/16.jpg"></div>
+
+        </div>
+        <div class="product_col products-items-grid_col">
+            <div class="product_img"><img src="../img/products-work/17.jpg" class="work-img expand-img" data-full="../img/products-work/full/17.jpg"></div>
+
+        </div>
+    </div>
+
+
+    <footer style="margin-top: auto">
+        <div class="wrap">
+            <hr>
+        </div>
+        <div class="wrap footer">
+            <img src="../img/logo2.png" alt="alt" style="height: 70px; width: 70px; order: 2;">
+            <div class="logo logo-footer" style="color: #2c3e50 !important; order: 1;"><span>SVE</span>T-SKG</div>
+            <div style="order: 3;">Разработано командой <a href="http://nek-web.ru" style="color: #00bfd3; ">NekWeb</a>.</div>
+        </div>
+    </footer>
+    <script src="../js/main.js"></script>
+    <script src="../js/fullimg.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+ use PHPMailer\PHPMailer\PHPMailer;
+
+ use PHPMailer\PHPMailer\Exception;
+
+ 
+$router->get('/', function () use ($router) {
+    return view('index');
+});
+$router->get('/products', function () use ($router) {
+    return view('products');
+});
+$router->get('/product/{id}', function ($id) use ($router) {
+$view = "product-".$id;
+    return view($view);
+});
+
+$router->get('/photos/{id}', function ($id) use ($router) {
+$view = "photo-".$id;
+    return view($view);
+});
+$router->get('/photos', function () use ($router) {
+    return view('photos');
+});
+
+
+$router->post('/mail', function () use ($router) {
+    
+
+
+
+ $post_data = "secret=6LcKrGgUAAAAAEwYjkIcDLsT7P1hVpJcyokI9HIk&response=".$_POST['g-recaptcha-response'];
+
+     $ch = curl_init();
+
+     curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+
+     curl_setopt($ch, CURLOPT_POST, true);
+
+     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded; charset=utf-8', 'Content-Length: ' . strlen($post_data)));
+
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+
+     $response = curl_exec($ch);
+
+     $rep = json_decode($response);
+
+     curl_close($ch);
+
+if ($rep->success == true)
+
+  {
+
+
+
+ $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+
+                try {
+
+                    //$mail->SMTPDebug = 2;
+
+                   // $mail->Debugoutput = function($str, $level) {echo "debug level $level; message: $str";};
+
+                    $mail->Port = 465;
+
+                    $mail->isSMTP();                                      // Set mailer to use SMTP
+
+                    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+					
+					$mail->CharSet = 'UTF-8';
+					
+                    $mail->Host = "smtp.mail.ru";  // Specify main and backup SMTP servers
+
+                    $mail->Username = 'ooo-skg@mail.ru';                 // SMTP username
+
+                    $mail->Password = 'tdutybq0412';                           // SMTP password
+
+                    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+
+                    $mail->setFrom('ooo-skg@mail.ru', 'Mailer.');
+
+                    $mail->addAddress('ooo-skg@mail.ru', 'Mailer .');     // Add a recipient
+
+                    $mail->addReplyTo('ooo-skg@mail.ru', 'noreply');
+
+                    $mail->isHTML(true);                                  // Set email format to HTML
+
+                    $mail->Subject = 'New Message';
+
+                    $mail->Body    = "Name: ".$_POST['name'].", Tel: ".$_POST['tel'].", Mail: ".$_POST['mail'].", Message: ".$_POST['message'];
+
+                    $mail->AltBody = '';
+
+                    $mail->send();
+
+                    
+                } 
+				catch (Exception $e) {
+				
+                 
+                }
+
+  }
+  else {
+
+  }
+});
+
+
